300a311eec8c5c16f4433c108fbf36ab6faf24ca
ELMartin31611 <martinborja31611@gmail.com>
2026-06-11 05:14:31 -0500

    clase 11-06-2026 check

--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/data/remote/dto/UserDto.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/data/remote/dto/UserDto.kt
@@ -15,8 +15,9 @@ data class UserDto(
     @SerializedName("is_active")   val isActive:   Boolean,
     @SerializedName("date_joined") val dateJoined: String,
     @SerializedName("num_orders")  val numOrders:  Int,
+    @SerializedName("avatar_url")
+    val avatarUrl:  String? = null,    // ← nuevo campo
 )
-
 data class UserRequestDto(
     val username:   String,
     val email:      String,
@@ -51,6 +52,7 @@ fun UserDto.toDomain() = User(
     isActive   = isActive,
     dateJoined = dateJoined,
     numOrders  = numOrders,
+    avatarUrl   = avatarUrl,
 )
 
 fun UserPayload.toRequest() = UserRequestDto(
